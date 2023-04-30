a55f24f7d2bddc3888f995a24d2314944ee7ad3c
pragyan69 <prajyanqw@gmail.com>
2023-04-30 10:18:15 +0530

    second commit

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,14 @@
 print ("hello , welcome to the quiz game")
 
 playing = input("do you want to play? ")
+
+if playing != 'yes':
+    quit()
+
+print ("okay lets play this game")
+
+answer = input ("wht does cpu stand for? ")
+if(answer == "Central processing uint"):
+    print ("correct!")
+
+    
